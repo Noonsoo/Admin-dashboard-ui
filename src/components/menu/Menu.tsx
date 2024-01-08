facfb3ed42a4c55507a344a003f9371cd76a1e7f
Noonsoo <nwafornonso70@gmail.com>
2024-01-08 18:01:18 +0000

    resolving design issues

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -7,10 +7,10 @@ function Menu() {
       {menu.map((item) => (
         <div className="item" key={item.id}>
           <span className="title">{item.title}</span>
-
           {item.listItems.map((listItem) => (
             <Link to={listItem.url} className="listItem" key={listItem.id}>
               <img src={listItem.icon} alt="Home Image" />
+
               <span className="listItemTitle">{listItem.title}</span>
             </Link>
           ))}
